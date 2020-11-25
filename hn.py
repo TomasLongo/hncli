35b303b06a077bc0b269f786cd6cf8af05c8de59
Tomas Longo <longo.tomas@googlemail.com>
2020-11-25 15:11:51 +0100

    Get Top 15 stories and print em

--- hn.py
+++ hn.py
@@ -1,14 +1,22 @@
 import requests
-from rich import print
+from rich.console import Console
+from rich.text import Text
 
 
 topStories = " https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
 itemBaseURL = " https://hacker-news.firebaseio.com/v0/item/"
+
 resp = requests.get(topStories)
+
+cons = Console()
 
 itemIDs = resp.json()
 
-for itemID in itemIDs[:5]:
+for itemID in itemIDs[:15]:
     itemResponse = requests.get(f'{itemBaseURL}/{itemID}/.json')
     json = itemResponse.json()
-    print(f'[bold magenta]{json["title"]}[/bold magenta]')
+
+    text = Text(str(itemID), style="magenta")
+    text.append(f' {json["title"]}', style="yellow")
+
+    cons.print(text)
