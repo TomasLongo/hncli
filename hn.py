0bba0b2d8f959d6258164c98ce379d0eea29121f
Tomas Longo <longo.tomas@googlemail.com>
2020-11-26 07:32:05 +0100

    Open story in browser

--- hn.py
+++ hn.py
@@ -1,22 +1,55 @@
+#! /usr/bin/python3
+
+import sys
 import requests
+import webbrowser
 from rich.console import Console
 from rich.text import Text
 
+options = sys.argv[1:]
 
 topStories = " https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
 itemBaseURL = " https://hacker-news.firebaseio.com/v0/item/"
 
-resp = requests.get(topStories)
 
-cons = Console()
+def exitPeacefully():
+    sys.exit(0)
 
-itemIDs = resp.json()
 
-for itemID in itemIDs[:15]:
-    itemResponse = requests.get(f'{itemBaseURL}/{itemID}/.json')
-    json = itemResponse.json()
+def fetchStory(id):
+    resp = requests.get(f'{itemBaseURL}/{id}.json')
+    return resp.json()
 
-    text = Text(str(itemID), style="magenta")
-    text.append(f' {json["title"]}', style="yellow")
 
-    cons.print(text)
+def fetchTopStories():
+    resp = requests.get(topStories)
+
+    cons = Console()
+
+    itemIDs = resp.json()
+
+    for itemID in itemIDs[:20]:
+        itemResponse = requests.get(f'{itemBaseURL}/{itemID}/.json')
+        json = itemResponse.json()
+
+        text = Text(str(itemID), style="magenta")
+        text.append(f' {json["title"]}', style="yellow")
+
+        cons.print(text)
+
+
+# fetch the story with passed id and open its url in the browser
+def openInBrowser(itemID):
+    item = fetchStory(itemID)
+    webbrowser.open_new_tab(item["url"])
+    exitPeacefully()
+
+
+if len(options) == 0:
+    fetchTopStories()
+    exitPeacefully()
+
+command = options[0]
+
+if command == "open":
+    openInBrowser(options[1])
